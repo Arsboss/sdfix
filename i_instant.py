eb05a64de5bd1be025cc8f193df6c5c6793f1fe8
CentralDeveloper228 <arsboss2017@yandex.ru>
2024-06-30 19:49:23 +0300

    Fixes

--- i_instant.py
+++ i_instant.py
@@ -74,7 +74,7 @@ def bui():
             if shared.opts.auto_launch_browser == "Remote" or cmd_opts.autolaunch:
                 auto_launch_browser = True
             elif shared.opts.auto_launch_browser == "Local":
-                auto_launch_browser = not cmd_opts.webui_is_non_local
+                auto_launch_browser = not cmd_opts.li_is_non_local
 
         app, local_url, share_url = shared.demo.launch(
             share=cmd_opts.share,
